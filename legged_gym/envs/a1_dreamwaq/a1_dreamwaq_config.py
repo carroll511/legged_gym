e849eaba375e08b78d6c2e4d6ba3bd425fbfddae
Carroll <carroll511@g.skku.edu>
2025-09-29 17:28:12 +0900

    final version w/o adaboot

--- legged_gym/envs/a1_dreamwaq/a1_dreamwaq_config.py
+++ legged_gym/envs/a1_dreamwaq/a1_dreamwaq_config.py
@@ -32,7 +32,7 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 
 class A1DreamWaQCfg( LeggedRobotCfg ):
     class env( LeggedRobotCfg.env ):
-        num_envs = 4096
+        num_envs = 2048
         num_observations = 45 # ang_vel(3), gravity(3), vel_cmd(3), joint_pos(12), joint_vel(12), last_action(12)
         num_privileged_obs = 238 # observations(45), height_map(187), disturbance(3) body_vel(3)
         num_actions = 12
