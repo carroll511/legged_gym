b67f691b7903ed580e1d38f967096e512c9576ac
carroll <carroll@kaist.ac.kr>
2025-10-21 15:52:41 +0900

    save

--- legged_gym/envs/a1_dreamwaq/a1_dreamwaq.py
+++ legged_gym/envs/a1_dreamwaq/a1_dreamwaq.py
@@ -45,7 +45,7 @@ class A1DreamWaQ(LeggedRobot):
         super().__init__(cfg, sim_params, physics_engine, sim_device, headless)
         
         self.history_len = cfg.env.history_len
-        self.history_obs_buf = torch.zeros(self.num_envs, self.history_len + 1, self.num_obs-3, device=self.device, dtype=torch.float)
+        self.history_obs_buf = torch.zeros(self.num_envs, self.history_len + 1, self.num_obs, device=self.device, dtype=torch.float)
         
     def _init_buffers(self):
         super()._init_buffers()
@@ -96,13 +96,12 @@ class A1DreamWaQ(LeggedRobot):
         self.add_noise = self.cfg.noise.add_noise
         noise_scales = self.cfg.noise.noise_scales
         noise_level = self.cfg.noise.noise_level
-        noise_vec[:3] = noise_scales.lin_vel * noise_level * self.obs_scales.lin_vel
-        noise_vec[3:6] = noise_scales.ang_vel * noise_level * self.obs_scales.ang_vel
-        noise_vec[6:9] = noise_scales.gravity * noise_level
-        noise_vec[9:12] = 0. # commands
-        noise_vec[12:24] = noise_scales.dof_pos * noise_level * self.obs_scales.dof_pos
-        noise_vec[24:36] = noise_scales.dof_vel * noise_level * self.obs_scales.dof_vel
-        noise_vec[36:48] = 0. # previous actions
+        noise_vec[0:3] = noise_scales.ang_vel * noise_level * self.obs_scales.ang_vel
+        noise_vec[3:6] = noise_scales.gravity * noise_level
+        noise_vec[6:9] = 0. # commands
+        noise_vec[9:21] = noise_scales.dof_pos * noise_level * self.obs_scales.dof_pos
+        noise_vec[21:33] = noise_scales.dof_vel * noise_level * self.obs_scales.dof_vel
+        noise_vec[33:45] = 0. # previous actions
         # if self.cfg.terrain.measure_heights:
         #     noise_vec[45:232] = noise_scales.height_measurements* noise_level * self.obs_scales.height_measurements
         return noise_vec
@@ -150,8 +149,7 @@ class A1DreamWaQ(LeggedRobot):
             self._draw_debug_vis()
 
     def compute_observations(self):
-        current_observation = torch.cat((   self.base_lin_vel  * self.obs_scales.lin_vel,
-                                            self.base_ang_vel  * self.obs_scales.ang_vel,
+        current_observation = torch.cat((   self.base_ang_vel  * self.obs_scales.ang_vel,
                                             self.projected_gravity,
                                             self.commands[:, :3] * self.commands_scale,
                                             (self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos,
@@ -163,7 +161,7 @@ class A1DreamWaQ(LeggedRobot):
 
         # Privileged observations
         # add privileged body velocities
-        # current_observation = torch.cat((current_observation, self.base_lin_vel * self.obs_scales.lin_vel), dim=-1)
+        current_observation = torch.cat((current_observation, self.base_lin_vel * self.obs_scales.lin_vel), dim=-1)
 
         disturbance_force = self.root_states[:, 7:10] * self.obs_scales.lin_vel
         current_observation = torch.cat((current_observation, disturbance_force), dim=-1)
@@ -182,9 +180,34 @@ class A1DreamWaQ(LeggedRobot):
 
         # store history observations
         self.history_obs_buf = torch.roll(self.history_obs_buf, shifts=1, dims=1)
-        # print(self.obs_buf[:, 3:].shape)
-        # print(self.history_obs_buf.shape)
-        self.history_obs_buf[:, 0, :] = self.obs_buf[:, 3:]
+        self.history_obs_buf[:, 0, :] = self.obs_buf
+
+    def compute_reward(self):
+        """ Compute rewards
+            Calls each reward function which had a non-zero scale (processed in self._prepare_reward_function())
+            adds each terms to the episode sums and to the total reward
+        """
+        self.rew_buf[:] = 0.
+
+        reward_components = {}
+
+        for i in range(len(self.reward_functions)):
+            name = self.reward_names[i]
+            rew = self.reward_functions[i]() * self.reward_scales[name]
+            self.rew_buf += rew
+            self.episode_sums[name] += rew
+
+            reward_components[name] = rew.mean().item()
+        
+        self.extras["reward_components"] = reward_components
+
+        if self.cfg.rewards.only_positive_rewards:
+            self.rew_buf[:] = torch.clip(self.rew_buf[:], min=0.)
+        # add termination reward after clipping
+        if "termination" in self.reward_scales:
+            rew = self._reward_termination() * self.reward_scales["termination"]
+            self.rew_buf += rew
+            self.episode_sums["termination"] += rew
 
     def get_history_observations(self):
         return self.history_obs_buf
