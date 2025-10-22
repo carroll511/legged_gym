208749eb08d9f270575cd245160694f805a2d942
carroll <carroll@kaist.ac.kr>
2025-10-22 21:16:28 +0900

    feet higher, stumble penalty 10/22-21_16

--- legged_gym/envs/a1_dreamwaq/a1_dreamwaq.py
+++ legged_gym/envs/a1_dreamwaq/a1_dreamwaq.py
@@ -218,21 +218,6 @@ class A1DreamWaQ(LeggedRobot):
         obs, privileged_obs, history_obs, _, _, _ = self.step(torch.zeros(self.num_envs, self.num_actions, device=self.device, requires_grad=False))
         return obs, privileged_obs, history_obs
     
-    def update_command_curriculum(self, env_ids):
-        """ Implements a curriculum of increasing commands
-
-        Args:
-            env_ids (List[int]): ids of environments being reset
-        """
-        # If the tracking reward is above 80% of the maximum, increase the range of commands
-        if (self.cfg.commands.curriculum_x and torch.mean(self.episode_sums["tracking_lin_vel"][env_ids]) / self.max_episode_length > self.cfg.commands.passing_grade_x * self.reward_scales["tracking_lin_vel"]):
-            self.command_ranges["lin_vel_x"][0] = np.clip(self.command_ranges["lin_vel_x"][0] - 0.5, -self.cfg.commands.max_curriculum_x, 0.)
-            self.command_ranges["lin_vel_x"][1] = np.clip(self.command_ranges["lin_vel_x"][1] + 0.5, 0., self.cfg.commands.max_curriculum_x)
-
-        if (self.cfg.commands.curriculum_yaw and torch.mean(self.episode_sums["tracking_ang_vel"][env_ids]) / self.max_episode_length > self.cfg.commands.passing_grade_yaw * self.reward_scales["tracking_ang_vel"]):
-            self.command_ranges["ang_vel_yaw"][0] = np.clip(self.command_ranges["ang_vel_yaw"][0] - 0.5, -self.cfg.commands.max_curriculum_yaw, 0.)
-            self.command_ranges["ang_vel_yaw"][1] = np.clip(self.command_ranges["ang_vel_yaw"][1] + 0.5, 0., self.cfg.commands.max_curriculum_yaw)
-
     def reset_idx(self, env_ids):
         """ Reset some environments.
             Calls self._reset_dofs(env_ids), self._reset_root_states(env_ids), and self._resample_commands(env_ids)
@@ -249,9 +234,7 @@ class A1DreamWaQ(LeggedRobot):
         if self.cfg.terrain.curriculum:
             self._update_terrain_curriculum(env_ids)
         # avoid updating command curriculum at each step since the maximum command is common to all envs
-        # if self.cfg.commands.curriculum and (self.common_step_counter % self.max_episode_length==0):
-        #     self.update_command_curriculum(env_ids)
-        if (self.cfg.commands.curriculum_x or self.cfg.commands.curriculum_yaw) and (self.common_step_counter % self.max_episode_length==0):
+        if self.cfg.commands.curriculum and (self.common_step_counter % self.max_episode_length==0):
             self.update_command_curriculum(env_ids)
         
         # reset robot states
@@ -275,10 +258,8 @@ class A1DreamWaQ(LeggedRobot):
         # log additional curriculum info
         if self.cfg.terrain.curriculum:
             self.extras["episode"]["terrain_level"] = torch.mean(self.terrain_levels.float())
-        if self.cfg.commands.curriculum_x:
+        if self.cfg.commands.curriculum:
             self.extras["episode"]["max_command_x"] = self.command_ranges["lin_vel_x"][1]
-        if self.cfg.commands.curriculum_yaw:
-            self.extras["episode"]["max_command_yaw"] = self.command_ranges["ang_vel_yaw"][1]
         # send timeout info to the algorithm
         if self.cfg.env.send_timeouts:
             self.extras["time_outs"] = self.time_out_buf
--- legged_gym/envs/a1_dreamwaq/a1_dreamwaq_config.py
+++ legged_gym/envs/a1_dreamwaq/a1_dreamwaq_config.py
@@ -43,41 +43,41 @@ class A1DreamWaQCfg( LeggedRobotCfg ):
         measure_heights = True
         slope_threshold = 0.38
 
-    # class commands:
-    #     curriculum = False
-    #     max_curriculum = 1.
-    #     num_commands = 3 # lin_vel_x, lin_vel_y, ang_vel_yaw
-    #     resampling_time = 10.
-    #     heading_command = False # ang_vel_yaw command
-    #     class ranges:
-    #         lin_vel_x = [-1.0, 1.0]
-    #         lin_vel_y = [-1.0, 1.0]
-    #         ang_vel_yaw = [-1, 1]
-
     class commands:
-        curriculum_x = True
-        curriculum_yaw = True
-        max_curriculum_x = 1.0
-        max_curriculum_y = 0.4
-        max_curriculum_yaw = 0.6
-
-        # Passing grade should be in [0,1]
-        passing_grade_x = 0.9
-        passing_grade_yaw = 0.4
-        inc_curriculum = 0.05
-
-        num_commands = 3  # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
-        resampling_time = 10.0  # time before command are changed[s]
-        heading_command = False  # if true: compute ang vel command from heading error
-
+        curriculum = False
+        max_curriculum = 1.
+        num_commands = 3 # lin_vel_x, lin_vel_y, ang_vel_yaw
+        resampling_time = 10.
+        heading_command = False # ang_vel_yaw command
         class ranges:
-            lin_vel_x = [-0.0, 0.0]
-            lin_vel_y = [-0.4, 0.4]
-            ang_vel_yaw = [-0.0, 0.0]
+            lin_vel_x = [-1.0, 1.0]
+            lin_vel_y = [-1.0, 1.0]
+            ang_vel_yaw = [-1, 1]
 
-        use_zero_command = False
-        if use_zero_command:
-            zero_command_prob = 0.1
+    # class commands:
+    #     curriculum_x = True
+    #     curriculum_yaw = True
+    #     max_curriculum_x = 1.0
+    #     max_curriculum_y = 0.4
+    #     max_curriculum_yaw = 0.6
+
+    #     # Passing grade should be in [0,1]
+    #     passing_grade_x = 0.9
+    #     passing_grade_yaw = 0.4
+    #     inc_curriculum = 0.05
+
+    #     num_commands = 3  # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
+    #     resampling_time = 10.0  # time before command are changed[s]
+    #     heading_command = False  # if true: compute ang vel command from heading error
+
+    #     class ranges:
+    #         lin_vel_x = [-0.0, 0.0]
+    #         lin_vel_y = [-0.4, 0.4]
+    #         ang_vel_yaw = [-0.0, 0.0]
+
+    #     use_zero_command = False
+    #     if use_zero_command:
+    #         zero_command_prob = 0.1
 
     class init_state( LeggedRobotCfg.init_state ):
         pos = [0.0, 0.0, 0.42] # x,y,z [m]
@@ -160,7 +160,7 @@ class A1DreamWaQCfg( LeggedRobotCfg ):
     class rewards( LeggedRobotCfg.rewards ):
         soft_dof_pos_limit = 0.9
         base_height_target = 0.4 # 0.25 -> 0.4
-        foot_height_target = 0.1 # 0.07 -> 0.1
+        foot_height_target = 0.15 # 0.07 -> 0.1 -> 0.15
         soft_dof_vel_limit = 0.65 # 1. -> 0.65
         soft_dof_pos_limit = 0.90 # 1. -> 0.90
         max_contact_force = 200. # 100. -> 200.
@@ -174,6 +174,7 @@ class A1DreamWaQCfg( LeggedRobotCfg ):
             foot_clearance = -0.01
             smoothness = -0.01
             power_distribution = -1e-5
+            stumble = -0.2
 
     class normalization:
         class obs_scales:
